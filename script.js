a9c12c0db3d33f8182bd3f3d01e44354524e966a
Natasha Limeres <natashaslimeres@gmail.com>
2022-09-12 19:00:37 -0300

    Criando função para inserir uma nova tarefa

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 'use strict';
 
-/* <label class="to-do-item">
-<input type="checkbox">
-<div class="text">teste de item 1</div>
-<input type="button" value="X">
-</label> */
+let banco = [
+    {'tarefa': 'Estudar JS', 'status': ''},
+    {'tarefa': 'Netflix', 'status': 'checked'},
+    {'tarefa': 'Fazer comida', 'status': 'checked'}
+]
 
-const criarItem = (tarefa) => {
+const criarItem = (tarefa, status) => {
     const item = document.createElement('label');
     item.classList.add("to-do-item");
     item.innerHTML = `
-       <input type="checkbox">
+       <input type="checkbox" ${status}>
        <div>${tarefa}</div>
        <input type="button" value="X">
        `
 
     document.getElementById("todoList").appendChild(item);
 }   
+
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild){
+    todoList.removeChild(todoList.lastChild)
+}
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    banco.forEach (item => criarItem (item.tarefa, item.status));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        banco.push({'tarefa': texto, 'status': 'checked'});
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress' ,inserirItem);
+
+atualizarTela();
+
